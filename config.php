50a00b1e3ac6f850f5e6ede2dffd1c7ed3110b5a
Chonsi <murolopezjosue48@gmail.com>
2026-05-31 20:11:55 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,7 @@
 <?php
-// Sustituye con tus datos reales de InfinityFree
 $host = "sql211.infinityfree.com"; 
 $user = "if0_42065518";
-$pass = "TU_CONTRASEÑA_DE_CPANEL"; // La contraseña que pusiste al crear la cuenta
+$pass = "Choncito2323"; 
 $db   = "if0_42065518_sistema_prestamos";
 
 $conn = new mysqli($host, $user, $pass, $db);
